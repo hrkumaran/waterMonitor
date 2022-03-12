e19b3221a9113c2ae42d3e2718a8b519e6e1c9f9
hrkumaran <kumaranbecse@gmail.com>
2022-03-12 17:09:31 +0530

    lombok

--- src/main/java/com/intuit/jobsearch/waterMonitor/model/WaterReadingDetails.java
+++ src/main/java/com/intuit/jobsearch/waterMonitor/model/WaterReadingDetails.java
@@ -1,10 +1,12 @@
 package com.intuit.jobsearch.waterMonitor.model;
 
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.usertype.UserType;
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
 @Entity(name = "WATER_READING_DETAILS")
 public class WaterReadingDetails {
 
@@ -28,51 +30,5 @@ public class WaterReadingDetails {
     @Column(name = "BILLEDMONTH")
     private Date billedMonth;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getWaterConsumptionLitres() {
-        return waterConsumptionLitres;
-    }
-
-    public void setWaterConsumptionLitres(Long waterConsumptionLitres) {
-        this.waterConsumptionLitres = waterConsumptionLitres;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public Date getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(Date creationTime) {
-        this.creationTime = creationTime;
-    }
-
-    public Date getUpdatedTime() {
-        return updatedTime;
-    }
-
-    public void setUpdatedTime(Date updatedTime) {
-        this.updatedTime = updatedTime;
-    }
-
-    public Date getBilledMonth() {
-        return billedMonth;
-    }
-
-    public void setBilledMonth(Date billedMonth) {
-        this.billedMonth = billedMonth;
-    }
 }
